bf97259098980199a159e3eafb674d63f2d08f27
Andrey <kaadron@yandex.ru>
2024-10-10 22:13:45 +0300

    переписал динамические классы с библиотеой classname

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,33 +1,20 @@
 import styleButton from './Button.module.scss';
+import classnames from 'classnames';
 
 const Button = ({ text = '', tab = '' }) => {
-  let border;
+  const {
+    button,
+    button__leftRadius: leftRadius,
+    button__rightRadius: rightRadius,
+    button__buttonMore: buttonMore,
+  } = styleButton;
 
-  if (tab === 'tab1') {
-    border = {
-      borderTopLeftRadius: '5px',
-      borderBottomLeftRadius: '5px',
-    };
-  } else if (tab === 'tab3') {
-    border = {
-      borderTopRightRadius: '5px',
-      borderBottomRightRadius: '5px',
-    };
-  } else if (tab === 'more') {
-    border = {
-      borderRadius: '5px',
-      backgroundColor: '#2196f3',
-      color: '#ffffff',
-    };
-  }
-
-  return (
-    <button
-      style={border}
-      className={styleButton.button}
-    >
-      {text}
-    </button>
+  const borderButton = classnames(
+    button,
+    tab === 'tab1' && leftRadius,
+    tab === 'tab3' && rightRadius,
+    tab === 'more' && buttonMore
   );
+  return <button className={borderButton}>{text}</button>;
 };
 export { Button };
