1225e5577342c1897da04575bf04ecc617fc907b
Andrey <kaadron@yandex.ru>
2024-11-11 21:33:20 +0300

    удаление лишнего

--- src/features/rootRedusers.ts
+++
@@ -1,9 +0,0 @@
-// import { combineReducers } from 'redux';
-// // import reduserCheckBox from './checkBox/checkBoxsSlice';
-// import reducerTickets from './tickets/ticketsSlice';
-
-// const rootRedusers = combineReducers({
-//   // checkBox: reduserCheckBox,
-//   tickets: reducerTickets,
-// });
-// export default rootRedusers;
