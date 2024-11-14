aa1659191ee67e39d8f0d7d7653603de340de9c3
andrey <kaadron@yandex.ru>
2024-11-14 14:41:47 +0300

    правка в логике

--- src/components/TicketField/TicketField.tsx
+++ src/components/TicketField/TicketField.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from '../../hooks/hooks';
 import {
   allSelected,
   OneTicket,
+  selectCheckBox,
   visibleTicketsSelector,
 } from '../../features/tickets/ticketsSlice';
 import { helperFilter } from '../../utils/helpers';
@@ -13,8 +14,8 @@ import { helperFilter } from '../../utils/helpers';
 const TicketField: React.FC = () => {
   const ticketsAll = useAppSelector(allSelected);
   const visibleTickets = useAppSelector(visibleTicketsSelector);
-  const allCheckBox = useAppSelector((state) => state.checkBox);
-
+  const allCheckBox = useAppSelector(selectCheckBox);
+  let id = 0;
   const { all, noTransfers, oneTransfers, twoTransfers, threeTransfers } = allCheckBox;
 
   const isChekedConditionFalse = () => {
@@ -46,9 +47,9 @@ const TicketField: React.FC = () => {
       {!isChekedConditionFalse() &&
         filterTicketsByCheckboxes(ticketsAll)
           .slice(0, visibleTickets)
-          .map((ticket: OneTicket, index: number) => (
+          .map((ticket: OneTicket) => (
             <Ticket
-              key={`${ticket.price}${index}`}
+              key={`${ticket.price}${++id}`}
               {...ticket}
             />
           ))}
